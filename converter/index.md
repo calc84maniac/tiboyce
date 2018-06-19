b7af86e44cc7af3b903c089ea339fd456e99b952
Brendan Fletcher <calc84maniac@gmail.com>
2018-06-18 22:47:36 -0400

    Add ROM converter for testing, fix blog link highlighting in sidebar.

---
+++ converter/index.md
@@ -0,0 +1,33 @@
+---
+layout: page
+title: ROM Converter
+sidebar_link: false
+---
+
+<link rel="stylesheet" href="{{ site.baseurl }}/assets/css/bootstrap.css">
+<link rel="stylesheet" href="{{ site.baseurl }}/assets/css/bootstrap-theme.css">
+<style>
+    #buttonsContainer button { margin-right: 5px; }
+    #dlList li { display: inline-block; }
+    #dlList li:before { content: '\2022'; margin-left: 0.5em; margin-right: 0.25em; }
+</style>
+
+Create .8xv files from your Game Boy ROM file: <input style="display: inline-block" type="file" accept=".gb,.gbc,.rom,.zip" onChange="fileLoad(event)"/>
+
+File links will appear here (click to download):
+<ul id="dlList"></ul>
+
+<div id="buttonsContainer"></div>
+
+-----
+
+*Online version implemented with [Emscripten](http://emscripten.org/) by [Adriweb](https://github.com/adriweb).*
+
+*Files are converted in-browser, no copyrighted data will be uploaded.*
+
+<script src="romgen-utils.js"></script>
+<script>
+    var script = document.createElement('script');
+    script.src = "romgen.js";
+    document.body.appendChild(script);
+</script>
---
+++ converter/romgen-utils.js
@@ -0,0 +1,173 @@
+/* TI-Boy CE online ROM converter - helper script - Adriweb */
+
+(function() {
+
+    var buttonsContainer = document.getElementById("buttonsContainer");
+    var dlList = document.getElementById("dlList");
+
+    var addBlobFileLink = function(file, name) {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.href = window.URL.createObjectURL((new Blob([file], {type: 'application/octet-stream'})));
+        a.download = name;
+        a.innerText = name;
+        li.appendChild(a);
+        dlList.appendChild(li);
+    };
+
+    var makeCompressedAndDownload = function(buttonLink, mode, fileName, romTitle, inputFileName, outputPrefix)
+    {
+        Module.callMain([ mode, '-t', romTitle, inputFileName, outputPrefix ]);
+        var file = FS.readFile(fileName, {encoding: 'binary'});
+        if (file) {
+            buttonLink.onclick = null;
+            buttonLink.href = window.URL.createObjectURL((new Blob([file], {type: 'application/octet-stream'})));
+            buttonLink.download = fileName;
+            buttonLink.click();
+        } else {
+            alert('Oops, something went wrong converting the ROM (see console)');
+            return;
+        }
+    };
+
+    fileLoaded = function(event, inputFileName)
+    {
+        dlList.innerHTML = buttonsContainer.innerHTML = "";
+
+        if (event.target.readyState === FileReader.DONE)
+        {
+            var fnameNoExt = inputFileName.replace(/\.(gb|gbc|rom|zip)$/, '');
+            var outputPrefix = fnameNoExt.replace(/[^A-Z0-9]/gi, '');
+            outputPrefix = (outputPrefix.substr(0, 1).toUpperCase() + outputPrefix.substr(1)).substr(0, 5);
+            var savedOutputPrefix = outputPrefix;
+            var cond = '(5 alphanumerical characters max, 1st must be uppercase letter)';
+            do {
+                if (outputPrefix.length === 0 || (! /[A-Z][a-zA-Z0-9]{0,4}/.test(outputPrefix))) {
+                    alert(cond);
+                }
+                outputPrefix = prompt("Enter the output filename prefix - " + cond, savedOutputPrefix);
+            } while (! /[A-Z][a-zA-Z0-9]{0,4}/.test(outputPrefix));
+
+            var romTitle = fnameNoExt;
+            do {
+                romTitle = prompt("Enter the ROM title to display in TI-Boy CE (ASCII characters)", fnameNoExt);
+            } while (romTitle.length && (! /^[\x20-\x7F]{0,255}$/.test(romTitle)));
+
+            FS.writeFile(inputFileName, new Uint8Array(event.target.result), {encoding: 'binary'});
+
+            var cleanupFiles = function()
+            {
+                try { FS.unlink(outputPrefix + ".8xv"); } catch (e){}
+                for (var i=0; i<50; i++) {
+                    try { FS.unlink(outputPrefix + "R" + ('00'+i).slice(-2) + ".8xv"); } catch (e){}
+                }
+            }
+
+            Module.callMain([ '-t', romTitle, inputFileName, outputPrefix ]);
+
+            // Display meta file
+            var fileName = outputPrefix + ".8xv";
+            var file = FS.readFile(fileName, {encoding: 'binary'});
+            if (file) {
+                addBlobFileLink(file, fileName);
+            } else {
+                alert('Oops, something went wrong converting the ROM (see console)');
+                return;
+            }
+
+            // Display part files
+            for (var i=0; i<50; i++) {
+                try {
+                    fileName = outputPrefix + "R" + ('00'+i).slice(-2) + ".8xv";
+                    file = FS.readFile(fileName, {encoding: 'binary'});
+                    if (file) {
+                        addBlobFileLink(file, fileName);
+                    } else {
+                        break;
+                    }
+                } catch (e) {
+                    console.log('[Error] Oops, probably reached the end of the files ' + e.message, fileName);
+                    break;
+                }
+            }
+
+            // Launch a b84 download
+            var btn = document.createElement("a");
+            btn.href = "#";
+            btn.className = "btn btn-default";
+            btn.innerHTML = "<span class='glyphicon glyphicon-compressed' aria-hidden='true'></span> Download TI-84 Plus CE bundle";
+            btn.onclick = (function(btn) {
+                return function(event) {
+                    cleanupFiles();
+                    makeCompressedAndDownload(btn, '-b84', outputPrefix+".b84", romTitle, inputFileName, outputPrefix);
+                    event.stopPropagation();
+                    return false;
+                }
+            })(btn);
+            buttonsContainer.appendChild(btn);
+
+            // Launch a b83 download
+            var btn = document.createElement("a");
+            btn.href = "#";
+            btn.className = "btn btn-default";
+            btn.innerHTML = "<span class='glyphicon glyphicon-compressed' aria-hidden='true'></span> Download TI-83 Premium CE bundle";
+            btn.onclick = (function(btn) {
+                return function(event) {
+                    cleanupFiles();
+                    makeCompressedAndDownload(btn, '-b83', outputPrefix+".b83", romTitle, inputFileName, outputPrefix);
+                    event.stopPropagation();
+                    return false;
+                }
+            })(btn);
+            buttonsContainer.appendChild(btn);
+
+            buttonsContainer.appendChild(document.createElement("br"));
+
+            // Add download buttons
+            if (/firefox/i.test(navigator.userAgent))
+            {
+                // Launch all downloads
+                var btn = document.createElement("btn");
+                btn.href = "#";
+                btn.className = "btn btn-default";
+                btn.innerHTML = "<span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> Launch downloads (all files)";
+                btn.onclick = function() {
+                    [].forEach.call(document.querySelectorAll('#dlList a'), function (a) { a.click(); });
+                };
+                buttonsContainer.appendChild(btn);
+            }
+
+            // Launch a zip download
+            var btn = document.createElement("a");
+            btn.href = "#";
+            btn.className = "btn btn-default";
+            btn.innerHTML = "<span class='glyphicon glyphicon-compressed' aria-hidden='true'></span> Download all files in a .zip";
+            btn.onclick = (function(btn) {
+                return function(event) {
+                    cleanupFiles();
+                    makeCompressedAndDownload(btn, '-z', outputPrefix+".zip", romTitle, inputFileName, outputPrefix);
+                    event.stopPropagation();
+                    return false;
+                }
+            })(btn);
+            buttonsContainer.appendChild(btn);
+        }
+    };
+
+    fileLoad = function(event)
+    {
+        var file = event.target.files[0];
+        if (!file || !(/\.(gb|gbc|rom|zip)$/.test(file.name)))
+        {
+            alert('Error: a .gb/gbc/rom/zip file is needed');
+            return;
+        }
+
+        var reader = new FileReader();
+        reader.onloadend = function(event) {
+            fileLoaded(event, file.name);
+        };
+        reader.readAsArrayBuffer(file);
+    };
+
+})()
