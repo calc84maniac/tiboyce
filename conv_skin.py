d2b595f1a1703a0ad41aaf64cd308e741499c691
Brendan Fletcher <calc84maniac@gmail.com>
2019-02-15 23:50:52 -0500

    Update conv_skin.py to use byte objects as they were intended.

--- conv_skin.py
+++ conv_skin.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python
 from PIL import Image
 import lzf
+import struct
 
 
 def compress(data):
-    return bytes(chr(len(data) % 256), 'latin1') \
-        + bytes(chr(len(data) // 256), 'latin1') \
-        + lzf.compress(data)
+    return struct.pack('<H', len(data)) + lzf.compress(data)
 
 
 img = Image.open('gb_skin.png')
 
-palette = b''
+palette = bytearray()
 pal = img.getpalette()
 for (r, g, b) in zip(pal[0:48:3], pal[1:48:3], pal[2:48:3]):
     color = ((g & 4) << 13) + ((b >> 3) << 10) + ((g >> 3) << 5) + (r >> 3)
-    palette += bytes(chr(color % 256), 'latin1') \
-        + bytes(chr(color // 256), 'latin1')
+    palette += struct.pack('<H', color)
 
-data = b''
+data = bytearray()
 last_pixel = None
 for pixel in (img.getpixel((x, y)) for y in range(img.height) for x in
               range(img.width)):
     if last_pixel is None:
         last_pixel = pixel
     else:
-        data += bytes(chr(last_pixel * 16 + pixel), 'latin1')
+        data.append(last_pixel * 16 + pixel)
         last_pixel = None
 
 if last_pixel is not None:
-    data += bytes(chr(last_pixel * 16), 'latin1')
+    data.append(last_pixel * 16)
 
-data = palette + compress(data)
+data = palette + compress(bytes(data))
 
 print(len(data))
 
@@ -39,10 +37,10 @@ fout = open('skin.asm', 'w')
 line = '.db '
 comma = 0
 for byte in data:
-    to_append = ',' * comma + '$%02X' % ord(chr(byte))
+    to_append = ',' * comma + '$%02X' % byte
     if len(line + to_append) > 100:
         fout.write(line + '\n')
-        line = '.db $%02X' % ord(chr(byte))
+        line = '.db $%02X' % byte
     else:
         line += to_append
     comma = 1
