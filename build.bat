3261e5245cf59a5856aeeb052da12c8fad9e02de
Zachary Wassall <runer112@gmail.com>
2024-07-28 17:38:23 -0400

    Quote variable paths

--- build.bat
+++ build.bat
@@ -8,12 +8,12 @@ goto exit
 set "as=spasm -E -T -L -A"
 set "build=build"
 
-if not exist %build% mkdir %build%
+if not exist "%build%" mkdir "%build%"
 
 @echo on
-%as% -DVERSION="\"%version%\"" tiboyce.asm %build%/TIBoyDat.8xv || goto exit
-%as% launcher.asm %build%/TIBOYCE.8xp || goto exit
-%as% skin.asm %build%/TIBoySkn.8xv || goto exit
+%as% -DVERSION="\"%version%\"" tiboyce.asm "%build%/TIBoyDat.8xv" || goto exit
+%as% launcher.asm "%build%/TIBOYCE.8xp" || goto exit
+%as% skin.asm "%build%/TIBoySkn.8xv" || goto exit
 @echo off
 
 echo.
--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ version=$(git describe --tags --dirty=*)
 as="spasm -E -T -L -A"
 build="build"
 
-mkdir -p $build
+mkdir -p "$build"
 
-set -x
-$as -DVERSION="\"$version\"" tiboyce.asm $build/TIBoyDat.8xv
-$as launcher.asm $build/TIBOYCE.8xp
-$as skin.asm $build/TIBoySkn.8xv
+$as -DVERSION="\"$version\"" tiboyce.asm "$build/TIBoyDat.8xv"
+$as launcher.asm "$build/TIBOYCE.8xp"
+$as skin.asm "$build/TIBoySkn.8xv"
